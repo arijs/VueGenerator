a56dc8bf6e506817a925d460eab07931f28b3360
Rafael Hengles <rhengles@gmail.com>
2018-09-13 21:13:19 +0000

    outra página funcionando com outro componente raiz com formulário, com store, com validação, com o supremo com tudo

--- _node/config/default.js
+++ _node/config/default.js
@@ -1,6 +1,6 @@
 var scopes = {
 	App: { JS_GLOBAL: '_app$', PREFIX: 'app', COMP_PATH_PREFIX: 'app/comp/', NAME: 'App' },
-	VComp: { JS_GLOBAL: '_vcomp$', PREFIX: 'vcomp', COMP_URL: 'https://unpkg.com/@arijs/frontend@0.1.0/vcomp/', NAME: 'VComp' },
+	VComp: { JS_GLOBAL: '_vcomp$', PREFIX: 'vcomp', COMP_URL: 'https://unpkg.com/@arijs/frontend@0.1.2/vcomp/', NAME: 'VComp' },
 	Global: { JS_GLOBAL: '_var$', NAME: 'Global' }
 };
 var scopesJson = [
@@ -34,8 +34,15 @@ module.exports = {
 		addBaseUrl_href: fnPrintBaseUrl('href', baseUrl),
 		addBaseUrl_src: fnPrintBaseUrl('src', baseUrl),
 		addBaseUrl_this: fnPrintBaseUrl(false, baseUrl),
+		json_stringify: function() {
+			return JSON.stringify(hop.call(this, '__json')?this.__json:this);
+		},
 		SCOPES: [scopes.App, scopes.VComp, scopes.Global],
 		SCOPES_JSON: scopesJson,
+		SCOPES_SET: [{
+			NAME: scopes.App.JS_GLOBAL+'.compInit',
+			VALUE: {__json:[{MOUNT:'#mount', ID:'app/root', SAVE:'$root'}]}
+		}],
 		PAGE_LANG: 'pt-br',
 		PAGE_TITLE: 'VueGenerator - NONE',
 		PAGE_DESCRIPTION: '',
@@ -49,21 +56,22 @@ module.exports = {
 			{ href: { addBaseUrl: 'css/mobile.css' }, media: '(max-width: 1024px)' }
 		],
 		FOOT_SCRIPTS_LIBS: [
-			{ src: 'https://unpkg.com/@arijs/frontend@0.1.1/utils/javascript.js' },
-			// { src: baseUrl+'vendor/js/utils/javascript.js' }, // use o local para testar alterações
-			{ src: 'https://unpkg.com/@arijs/frontend@0.1.1/utils/loaders.js' },
-			// { src: baseUrl+'vendor/js/utils/loaders.js' }, // ou enquanto o unpkg não reflete a última versão
-			{ src: 'https://unpkg.com/@arijs/frontend@0.1.1/utils/form.js' },
-			{ src: 'https://unpkg.com/@arijs/frontend@0.1.1/utils/string.js' },
-			{ src: 'https://unpkg.com/@arijs/frontend@0.1.1/utils/animation.js' },
-			{ src: 'https://unpkg.com/@arijs/vue@2.5.17-beta.0/dist/vue.min.js'},
+			{ src: 'https://unpkg.com/@arijs/frontend@0.1.2/utils/javascript.js' },
+			// { src: { addBaseUrl: 'vendor/js/utils/javascript.js' } }, // use o local para testar alterações
+			{ src: 'https://unpkg.com/@arijs/frontend@0.1.2/utils/loaders.js' },
+			// { src: { addBaseUrl: 'vendor/js/utils/loaders.js' } }, // ou enquanto o unpkg não reflete a última versão
+			{ src: 'https://unpkg.com/@arijs/frontend@0.1.2/utils/form.js' },
+			// { src: { addBaseUrl: 'vendor/js/utils/form.js' } },
+			{ src: 'https://unpkg.com/@arijs/frontend@0.1.2/utils/string.js' },
+			{ src: 'https://unpkg.com/@arijs/frontend@0.1.2/utils/animation.js' },
+			{ src: 'https://unpkg.com/@arijs/vue@2.5.17-beta0.0/dist/vue.js'},
+			// { src: { addBaseUrl: 'vendor/js/vue.js' } },
 			{ src: 'https://unpkg.com/vuex@3.0.1/dist/vuex.min.js' },
 			{ src: 'https://unpkg.com/vuebar@0.0.18/vuebar.js' }
 		],
 		FOOT_SCRIPTS_APP: [
 			{ src: { addBaseUrl: 'app/services.js' } },
-			{ src: { addBaseUrl: 'app/store.js' } },
-			{ src: { addBaseUrl: 'app/initialize.js' } }
+			{ src: { addBaseUrl: 'app/store.js' } }
 		]
 	},
 	pages: {
@@ -71,7 +79,10 @@ module.exports = {
 			template: null, // usar como padrão o nome da chave ("index" neste caso)
 			output: 'example.html', // como padrão, o nome da chave + ".html" ("index.html")
 			template_vars: {
-				PAGE_TITLE: 'VueGenerator - INDEX'
+				PAGE_TITLE: 'VueGenerator - INDEX',
+				FOOT_SCRIPTS_PAGE: [
+					{ src: { addBaseUrl: 'app/initialize.js' } }
+				]
 			}
 		},
 		outra_pagina: {
@@ -82,7 +93,15 @@ module.exports = {
 				addBaseUrl_src: fnPrintBaseUrl('src', baseUrlOutraPagina),
 				addBaseUrl_this: fnPrintBaseUrl(false, baseUrlOutraPagina),
 				BASE_URL_STRING: JSON.stringify(baseUrlOutraPagina()),
-				PAGE_TITLE: 'VueGenerator - OUTRA'
+				COMP_ROOT: 'app/exemplo-form',
+				PAGE_TITLE: 'VueGenerator - OUTRA',
+				SCOPES_SET: [{
+					NAME: scopes.App.JS_GLOBAL+'.compInit',
+					VALUE: {__json:[{MOUNT:'#mount', ID:'app/exemplo-form', SAVE:'$root'}]}
+				}],
+				FOOT_SCRIPTS_PAGE: [
+					{ src: { addBaseUrl: 'app/initialize-outra-pagina.js' } }
+				]
 			}
 		},
 		404: {
---
+++ app/comp/exemplo-form/exemplo-form.js
@@ -0,0 +1,47 @@
+(function() {
+	'use strict';
+	var vars = window._var$;
+	var App = window._app$;
+	var Utils = vars.Utils;
+	var reTrim = /^\s+|\s+$/g;
+	function validaNome(campo) {
+		var valor = campo.valor.replace(reTrim, '');
+		if (!/^[a-z ]+$/i.test(Utils.deaccentize(valor))) {
+			return { erro: 'Nome inválido' };
+		}
+	}
+	App.compMap["exemplo-form"] = function(callback) {
+		callback(null, comp);
+	};
+	var comp = {
+		data: function() {
+			return {
+				campoNome: {
+					nome: 'nome',
+					rotulo: 'Nome',
+					valor: '',
+					erro: null,
+					falta: false,
+					valido: false,
+					valida: [
+						Utils.valida.opcional,
+						validaNome
+					]
+				}
+			}
+		},
+		computed: {
+			greeting: function() {
+				return 'Hello '+(this.campoNome.valor || 'World')+'!';
+			}
+		},
+		methods: {
+			campoValorNome: function(payload) {
+				this.$store.dispatch('campoValor', payload);
+			},
+			campoBlur: function(payload) {
+				this.$store.dispatch('validarCampo', payload);
+			}
+		}
+	};
+})();
